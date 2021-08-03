02a5eccb027b6d3153c93fcf0274714d95d0a479
YoshikiHanashiro <e195733@ie.u-ryukyu.ac.jp>
2021-08-03 13:47:29 +0900

    coinのサンプルを修正、速度を比較できるようにmainを改良

--- code/main_sample.py
+++ code/main_sample.py
@@ -1,4 +1,17 @@
 from module import pool_parallel as po
 from module import random_sample as ras
-po.pool_pal(ras.coin, 4, 10)
-po.pool_pal(ras.dice, 4, 10)
+import time
+
+pool_time = time.time()
+po.pool_pal(ras.coin, 4, 100000)
+end_pool_time = time.time() - pool_time
+
+#po.pool_pal(ras.dice, 4, 10)
+
+time_no = time.time()
+for i in range(100000):
+    ras.coin(i)
+end_time = time.time() - time_no
+
+print ("並列化あり(pool):{0}".format(end_pool_time) + "[sec]")
+print ("並列化なし:{0}".format(end_time) + "[sec]")
--- code/module/random_sample.py
+++ code/module/random_sample.py
@@ -2,15 +2,14 @@ import time
 import random
 
 def coin(x):
-    for i in range(x):
-        print("num: %d" % x)
-        time.sleep(0.2)
-        coin_num = random.randint(0,1)
-        return(coin_num)
+    print("num: %d" % x)
+    #time.sleep(0.2)
+    coin_num = random.randint(0,1)
+    return(coin_num)
 
 def dice(x):
     for i in range(x):
-        print("num: %d" % x)
+        print("num: %d" %i)
         time.sleep(0.2)
         dice_num = random.randint(1,6)
         return(dice_num)
