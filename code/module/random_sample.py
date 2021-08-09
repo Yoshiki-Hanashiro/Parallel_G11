5ed4a5f271b63cd6e7b10f110b2509b7aa2b3fed
玉那覇　翔太 <e195751@ie.u-ryukyu.ac.jp>
2021-08-09 16:48:44 +0900

    commit

--- code/module/random_sample.py
+++ code/module/random_sample.py
@@ -13,13 +13,3 @@ def dice(x):
     dice_num = random.randint(1,6)
     return(dice_num)
 
-def find_divisor(number,R):
-    OK= 0
-    for k in range(R,number+1):
-        if number % k == 0:
-            OK = 1
-            print("number = {}".format(k))
-            break
-    return(OK)
-##約数の個数をカウントするメソッド　これは約数の一つを見つけたら終了する感じ
-##これをnumberの数だけ実行できるようにする
