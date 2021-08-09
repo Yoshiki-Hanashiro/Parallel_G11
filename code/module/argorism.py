cd3cf3f55bc19dadf9272e1f1947d2202448efb0
玉那覇　翔太 <e195751@ie.u-ryukyu.ac.jp>
2021-08-10 02:34:28 +0900

    平方数の加算を行った

--- code/module/argorism.py
+++ code/module/argorism.py
@@ -1,5 +1,6 @@
 import time
 import random
+from multiprocessing import Pool
 
 def coin(x):
     print("num: %d" % x)
@@ -13,13 +14,45 @@ def dice(x):
     dice_num = random.randint(1,6)
     return(dice_num)
 
-def find_divisor(number,R):
-    OK= 0
-    for k in range(R,number+1):
-        if number % k == 0:
-            OK = 1
-            print("number = {}".format(k))
-            break
-    return(OK)
-##約数の個数をカウントするメソッド　これは約数の一つを見つけたら終了する感じ
-##これをnumberの数だけ実行できるようにする
+def find_divisor(number):
+    Prime = False
+    for k in range(1,number):
+        if k == 1:
+            continue
+        elif number % k == 0:
+            Prime = True
+
+    return Prime
+
+def parallel(x):
+    return x**2
+
+def no_parallel(x):
+    return x**2
+
+
+##並列化あり
+if __name__ == "__main__":
+     for j in range(1,11):
+        p = Pool(j)
+        start = time.time()
+        x =sum(p.map(parallel,range(1,10000000)))
+        elapsed_time = time.time() - start
+        print("並列化(プロセス数:{}){}[sec]".format(j,elapsed_time))
+        print("count={}".format(x))
+##並列化無し
+     start = time.time()
+     count = 0
+     for i in range(1, 10000000):
+        count += no_parallel(i)
+     elapsed_time = time.time() - start
+     print("並列化無し:{0}[sec]".format(elapsed_time))
+     print("count={}".format(count))
+
+
+
+
+
+
+
+
