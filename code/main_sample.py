77dec61ac6462637708898c4324f881120b77d56
YoshikiHanashiro <e195733@ie.u-ryukyu.ac.jp>
2021-08-09 23:36:52 +0900

    最初部分のみ番号を表示するサンプル作成

--- code/main_sample.py
+++ code/main_sample.py
@@ -1,31 +1,24 @@
 # -*- coding: utf-8 -*-
 from module import pool_parallel as po
 from module import random_sample as ras
-from module import Process as pro
 import time
 
 # 100000
 pool_time = time.time()
-max_num = 100000
+max_num = 100
 result = po.pool_pal(ras.coin, 4, max_num)
 end_pool_time = time.time() - pool_time
 num0 = result.count(0)
 probability = num0 / max_num * 100
 
-process_time = time.time()
-process_result = pro.process(4, max_num)
-end_process_time = time.time() - process_time
-pro_num0 = process_result.count(0)
-pro_probability = pro_num0 / max_num * 100
-
 #po.pool_pal(ras.dice, 4, 10)
 
 time_no = time.time()
+coin_num = []
 for i in range(max_num):
-    ras.coin(i)
+    coin_num.append(ras.coin(i))
 end_time = time.time() - time_no
 
 print ("並列化あり(pool):{0}".format(end_pool_time) + "[sec]")
-print ("並列化あり(process):{0}".format(end_process_time) + "[sec]")
 print ("並列化なし:{0}".format(end_time) + "[sec]")
 print("表が出る確率:{0}".format(probability))
